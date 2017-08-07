eb8308ce5862d6d8a2e7716717a57911ca2b1dab
BoerniMacBook <hergenroeder@droxit.de>
2017-08-07 13:03:27 +0200

    added protocol buffer schema and set the first test values

--- mosaic/base_service.py
+++ mosaic/base_service.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3.5
 
+from mosaic.communication import service_com_pb2
+
 
 class BaseService:
     def __init__(self):
-        print('Yeah initiated the base service °!° ')
+        self.mosaic_message = service_com_pb2.MosaicMessage()
+        pipeline = service_com_pb2.Pipeline()
+
+        service = pipeline.services.add()
+        service.id = 'baseService-111elf'
+        parameter = service.parameters.add()
+        parameter.serviceParams = 'troll=lol'
+
+        payload = service_com_pb2.Payload()
+        payload.firstNumber = 2
+        payload.secondNumber = 2
+
+        self.mosaic_message.pipeline.CopyFrom(pipeline)
+        self.mosaic_message.payload.CopyFrom(payload)
+
+    def get_mosaic_message(self):
+        return self.mosaic_message
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 echo Starting tests...
+python3 -m unittest base_service.py &&
 echo ...Finished tests
