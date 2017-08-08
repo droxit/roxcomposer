46753827f011aa5603e922b0f5d71baaadd92f17
BoerniMacBook <hergenroeder@droxit.de>
2017-08-08 12:32:12 +0200

    updated test execution paths in shellscript

--- mosaic/tests/run_tests.sh
+++ mosaic/tests/run_tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo Starting tests...
-python3 -m unittest base_service.py &&
-python3 -m unittest math_service.py &&
-python3 -m unittest number_service.py &&
+python3 -m unittest tests/base_service.py &&
+python3 -m unittest tests/math_service.py &&
+python3 -m unittest tests/number_service.py &&
 echo ...Finished tests
