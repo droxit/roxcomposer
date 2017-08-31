9a9dcae4ec24d0356a06fce237ce9543862d76a5
Sebastian Bickerle <bickerle@droxit.de>
2017-08-31 13:02:52 +0200

    fix pipeline test

--- mosaic/tests/service_communication.py
+++ mosaic/tests/service_communication.py
@@ -5,14 +5,22 @@ import os
 import socket
 import signal
 import time
+from multiprocessing import Process
 from mosaic import base_service
 from mosaic.communication import mosaic_message
+
+def sigint_handler(signum, frame):
+    exit(0)
+
+def start_service(serv):
+    serv = AppendService(serv['msg'], serv['args'])
+    serv.listen()
+
 
 class AppendService(base_service.BaseService):
     def __init__(self, msg, args):
         self.msg = msg
         super().__init__(args)
-        self.listen()
 
     def on_message(self, msg):
         print(self.mosaic_message.get_protobuf_msg_as_dict())
@@ -22,24 +30,22 @@ class AppendService(base_service.BaseService):
 class TestPipeline(unittest.TestCase):
     def setUp(self):
         self.services = [
-            {'msg': 'service 1', 'args': {'name': 'service1', 'ip': '127.0.0.1', 'port': 10001}},
-            {'msg': 'service 2', 'args': {'name': 'service2', 'ip': '127.0.0.1', 'port': 10002}},
-            {'msg': 'service 3', 'args': {'name': 'service3', 'ip': '127.0.0.1', 'port': 10003}}
+            {'msg': 'service 1', 'args': {'name': 'service1', 'ip': '127.0.0.1', 'port': 10001, 'logging': {'level': 'DEBUG'}}},
+            {'msg': 'service 2', 'args': {'name': 'service1', 'ip': '127.0.0.1', 'port': 10002, 'logging': {'level': 'DEBUG'}}},
+            {'msg': 'service 3', 'args': {'name': 'service1', 'ip': '127.0.0.1', 'port': 10003, 'logging': {'level': 'DEBUG'}}},
         ]
 
         self.children = []
 
         for serv in self.services:
-            pid = os.fork()
-            if pid:
-                self.children.append(pid)
-            else:
-                serv = AppendService(serv['msg'], serv['args'])
+            p = Process(target=start_service, args=(serv,))
+            p.start()
+            self.children.append(p)
 
     def tearDown(self):
         if len(self.children):
-            for pid in self.children:
-                os.kill(pid, signal.SIGINT)
+            for p in self.children:
+                p.terminate()
 
     def test_pipeline(self):
         time.sleep(0.5)
@@ -55,22 +61,29 @@ class TestPipeline(unittest.TestCase):
         mm.set_content(payload)
         bin_msg = mosaic_message.Utils.serialize(mm.get_protobuf_msg())
 
-        sock = socket.socket()
-        sock.bind((ip,port))
 
-        sock.connect((self.services[0]['args']['ip'], self.services[0]['args']['port']))
-        sock.sendall(bin_msg)
-        resp = sock.recv(2048)
-        print(resp)
 
-        sock.listen()
-        conn, addr = sock.accept()
-        recv_msg = conn.recv(2048)
-        recv_msg = mosaic_message.Utils.deserialize(recv_msg)
-        recv_msg = mosaic_message.Message(recv_msg)
+        msg = ""
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.bind((ip,port))
+            s.listen()
+
+            conn = socket.create_connection((self.services[0]['args']['ip'], self.services[0]['args']['port']))
+            conn.sendall(bin_msg)
+            #conn.recv(1024)
+            conn.close()
+
+            conn, addr = s.accept()
+            with conn:
+                msg = conn.recv(2048)
+                conn.send(b'OK')
+
+        msg = mosaic_message.Utils.deserialize(msg)
+        msg = mosaic_message.Message(msg)
 
         # there should not be a pipeline cause it 
-        self.assertEqual(recv_msg.get_content(), expected_payload)
+        self.assertEqual(msg.get_content().body, expected_payload)
         
 
 if __name__ == '__main__':
