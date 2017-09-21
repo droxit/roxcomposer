0b0e36943d48719bf92f84661faf6fd7b517dc04
Sebastian Bickerle <bickerle@droxit.de>
2017-09-21 17:09:27 +0200

    add class loader exceptions

--- mosaic/exceptions.py
+++ mosaic/exceptions.py
@@ -15,3 +15,10 @@ class InvalidMosaicMessage(Exception):
 
     def __str__(self):
         return repr(self.value)
+
+class NotAClass(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
