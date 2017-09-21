8157f422775597ba7953ef1fd12534bf57e59f32
Sebastian Bickerle <bickerle@droxit.de>
2017-09-21 17:09:50 +0200

    add exceptions

--- mosaic/service_loader.py
+++ mosaic/service_loader.py
@@ -2,13 +2,21 @@
 
 import sys
 import importlib
+import inspect
+from mosaic import exceptions
 
 def load_class(classpath):
+    if classpath is None:
+        raise exceptions.ParameterMissing("classpath is empty")
+
     components = classpath.split('.')
     modpath = ".".join(components[:-1])
     classname = components[-1]
     mod = importlib.import_module(modpath)
     c = getattr(mod, classname)
+
+    if not inspect.isclass(c):
+        raise exceptions.NotAClass(classpath)
 
     return c
 
