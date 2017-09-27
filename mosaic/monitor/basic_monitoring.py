c9779eae4d9bafa6a2b3073b47c499f6d3b167de
BernhardHergenroeder <hergenroeder@droxit.de>
2017-09-27 16:32:45 +0200

    added msg_reached_final_destination function

--- mosaic/monitor/basic_monitoring.py
+++ mosaic/monitor/basic_monitoring.py
@@ -12,3 +12,6 @@ class BasicMonitoring:
 
     def msg_dispatched(self):
         print('MosaicMessage dispatched at', time.time())
+
+    def msg_reached_final_destination(self):
+        print('MosaicMessage reached final destination at', time.time())
