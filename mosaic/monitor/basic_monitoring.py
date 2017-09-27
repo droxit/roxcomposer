65831c7f588fcca2bef3e389b192efd2c55a923a
BernhardHergenroeder <hergenroeder@droxit.de>
2017-09-27 17:32:33 +0200

    updated format of monitoring messages to add every keyword a service is passing to the monitoring message

--- mosaic/monitor/basic_monitoring.py
+++ mosaic/monitor/basic_monitoring.py
@@ -11,25 +11,25 @@ class BasicMonitoring:
         self.arguments = kwargs
 
     # monitors msg receiving acitvities
-    def msg_received(self):
-        msg = 'MosaicMessage received at'
-        print(msg + str(time.time()))
+    def msg_received(self, **kwargs):
+        msg = '[' + str(kwargs) + ']' + ' MosaicMessage received at' + ' ' + str(time.time())
+        print(msg)
         self.write_to_file(msg)
 
     # monitors msg dispatching acitvities
-    def msg_dispatched(self):
-        msg = 'MosaicMessage dispatched at'
-        print(msg + str(time.time()))
+    def msg_dispatched(self, **kwargs):
+        msg = '[' + str(kwargs) + ']' + ' MosaicMessage dispatched at' + ' ' + str(time.time())
+        print(msg)
         self.write_to_file(msg)
 
     # monitors msgs which reached their final destination
-    def msg_reached_final_destination(self):
-        msg = 'MosaicMessage reached final destination at'
-        print(msg + str(time.time()))
+    def msg_reached_final_destination(self, **kwargs):
+        msg = '[' + str(kwargs) + ']' + ' MosaicMessage reached final destination at' + ' ' + str(time.time())
+        print(msg)
         self.write_to_file(msg)
 
     # a helper function to write to a file
     def write_to_file(self, msg):
         fh = open(self.arguments['filename'], 'a')
-        fh.write(msg + ' ' + str(time.time()) + '\n')
+        fh.write(msg + '\n')
         fh.close()
