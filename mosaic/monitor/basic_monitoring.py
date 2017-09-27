d8645901f9150adeebbc3aa5747712a0315f0fff
BernhardHergenroeder <hergenroeder@droxit.de>
2017-09-27 17:07:34 +0200

    added writing to file to monitoring

--- mosaic/monitor/basic_monitoring.py
+++ mosaic/monitor/basic_monitoring.py
@@ -3,15 +3,33 @@
 import time
 
 
+# This class yields a basic monitoring solution. It offers some functions to monitor service activities in a pipeline.
+# The basic version of the monitoring writes every information to one file, which is configurable by defining a para-
+# meter in the service parameters.
 class BasicMonitoring:
     def __init__(self, **kwargs):
         self.arguments = kwargs
 
+    # monitors msg receiving acitvities
     def msg_received(self):
-        print('MosaicMessage received at', time.time())
+        msg = 'MosaicMessage received at'
+        print(msg + str(time.time()))
+        self.write_to_file(msg)
 
+    # monitors msg dispatching acitvities
     def msg_dispatched(self):
-        print('MosaicMessage dispatched at', time.time())
+        msg = 'MosaicMessage dispatched at'
+        print(msg + str(time.time()))
+        self.write_to_file(msg)
 
+    # monitors msgs which reached their final destination
     def msg_reached_final_destination(self):
-        print('MosaicMessage reached final destination at', time.time())
+        msg = 'MosaicMessage reached final destination at'
+        print(msg + str(time.time()))
+        self.write_to_file(msg)
+
+    # a helper function to write to a file
+    def write_to_file(self, msg):
+        fh = open(self.arguments['filename'], 'a')
+        fh.write(msg + ' ' + str(time.time()) + '\n')
+        fh.close()
