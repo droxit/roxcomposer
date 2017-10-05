c735f921cc99fd1207b9de14ad404087959d306b
Sebastian Bickerle <bickerle@droxit.de>
2017-10-05 16:11:11 +0200

    add tests for logger class injection

--- mosaic/tests/basic_logger.py
+++ mosaic/tests/basic_logger.py
@@ -8,9 +8,9 @@ from mosaic.tests.classes.test_logging import LogTest
 from mosaic import exceptions
 
 
-class TestBaseService(unittest.TestCase):
+class TestLogging(unittest.TestCase):
  
-    def test_logging(self):
+    def test_basic_logger(self):
 
         with TemporaryDirectory() as tdir:
             log_path = join(tdir, 'logtest.log')
@@ -58,6 +58,30 @@ class TestBaseService(unittest.TestCase):
             #iso_timestamp_re = re.compile("^(?:[1-9]\d{3}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[1-9]\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00)-02-29)T(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d(?:Z|[+-][01]\d:[0-5]\d)$")
             #self.assertRegex(msg_in, iso_timestamp_re)
 
+            f.close()
+
+    def test_logging_injection(self):
+        with TemporaryDirectory() as tdir:
+            log_path = join(tdir, 'logtest.log')
+
+            params = {
+                'name': 'logtest',
+                'ip': 'not important',
+                'port': 7,
+                'logging': {
+                    'level': 'DEBUG',
+                    'filename': log_path,
+                    'logger_class': 'mosaic.tests.classes.dummy_logger.DummyLog'
+                }
+            }
+
+            lt = LogTest(params)
+            f = open(log_path, "r")
+
+            msg = 'simple message'
+            lt.logdebug(msg)
+            logline = f.readline()
+            self.assertEqual(msg, logline, "wrong log output")
             f.close()
 
 if __name__ == '__main__':
