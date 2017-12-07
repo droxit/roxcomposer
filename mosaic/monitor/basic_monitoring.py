38e6d8eeb4a56cd60c09a08566c7960557431896
Sebastian Bickerle <bickerle@droxit.de>
2017-12-07 18:43:58 +0100

    add basic reporting service

--- mosaic/monitor/basic_monitoring.py
+++ mosaic/monitor/basic_monitoring.py
@@ -1,6 +1,7 @@
 import time
+import json
 from mosaic import exceptions
-
+from mosaic import base_service
 
 def check_args(args, *required):
     for r in required:
@@ -70,5 +71,34 @@ class BasicReporting:
             return {"status": "message not found"}
 
             
+### expects JSON messages in the form {"function": "get_msg_status", "args": {...}}
+class BasicReportingService(base_service.BaseService):
+    def __init__(self, params):
+        super().__init__(params)
+        check_args(params, "filename")
+        self.reporter = BasicReporting(filename=params['filename'])
+
+    def on_message(self, msg):
+        try:
+            m = json.parse(msg)
+        except Exception as e:
+            errormsg = "unable to parse message, expecting JSON. msg: {}".format(msg)
+            self.logger.error(errormsg)
+            self.dispatch('{"error": "{}"}'.format(errormsg))
+
+        check_args(m, "function", "args")
+        try:
+            if args['function'] == "get_msg_status":
+                reply = self.reporter.get_msg_status(m['args'])
+            elif args['function'] == "get_msg_history":
+                reply = self.reporter.get_msg_history(m['args'])
+            else:
+                reply = {"error": "unsupported function: {}".format(args['function'])}
+        except Exception as e:
+            reply = {"error": str(e)}
+
+        dispatch(json.dumps(reply))
+
+
         
 
