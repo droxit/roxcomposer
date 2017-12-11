3357caed6b2cdd22e8049e6da9ceb5328a3beb95
Sebastian Bickerle <bickerle@droxit.de>
2017-12-11 15:11:44 +0100

    fix indentation

--- impress_plugin/test/test_integration.js
+++ impress_plugin/test/test_integration.js
@@ -14,13 +14,13 @@ let logger = bunyan.createLogger({
 			});
 
 describe('mosaic control integration tests', function () {
-    let tmpdir = fs.mkdtempSync('/tmp/mosaic_control_test_integration');
-    mc.init({logger: logger});
+	let tmpdir = fs.mkdtempSync('/tmp/mosaic_control_test_integration');
+	mc.init({logger: logger});
 	describe('service startup and control', function () {
-        let filepath = tmpdir + "/file_writer.out";
-        let filewriter_params = {name: 'fwriter', ip: '127.0.0.1', port: 6789, filepath: filepath};
-        let filewriter_module_path = '../mosaic/tests/classes/file_writer.py';
-        let test_msg = 'test _mesage from hell';
+		let filepath = tmpdir + "/file_writer.out";
+		let filewriter_params = {name: 'fwriter', ip: '127.0.0.1', port: 6789, filepath: filepath};
+		let filewriter_module_path = '../mosaic/tests/classes/file_writer.py';
+		let test_msg = 'test _mesage from hell';
 		it('should return no error when starting FileWriter with proper arguments', function (done) {
 			mc.start_service({path: filewriter_module_path, params: filewriter_params}, function (err, msg) {
 				if (err)
@@ -29,55 +29,55 @@ describe('mosaic control integration tests', function () {
 					done();
 			});
 		});
-        it('should list the service in the services', function () {
-            mc.get_services(null, function(err, msg) {
-                expect(err).to.be(null);
-                expect(msg).to.eql({fwriter: {path: filewriter_module_path, params: filewriter_params}});
-            });
+		it('should list the service in the services', function () {
+			mc.get_services(null, function(err, msg) {
+				expect(err).to.be(null);
+				expect(msg).to.eql({fwriter: {path: filewriter_module_path, params: filewriter_params}});
+			});
 		});
-        it('should be able to create a pipeline', function () {
+		it('should be able to create a pipeline', function () {
 			mc.set_pipeline({name: 'pipe', services: ['fwriter']}, function (err, msg) {
-                expect(err).to.be(null);
+				expect(err).to.be(null);
 			});
 		});
-        it('should list the pipeline in pipelines', function () {
+		it('should list the pipeline in pipelines', function () {
 			mc.get_pipelines(null, function (err, msg) {
-                expect(err).to.be(null);
-                expect(msg).to.eql({pipe: {services: ['fwriter'], active: true}});
+				expect(err).to.be(null);
+				expect(msg).to.eql({pipe: {services: ['fwriter'], active: true}});
 			});
 		});
-        it('should be able to post a message to the pipeline', function () {
-            sleep(400);
+		it('should be able to post a message to the pipeline', function () {
+			sleep(400);
 			mc.post_to_pipeline({name: 'pipe', data: test_msg}, function (err, msg) {
-                expect(err).to.be(null);
+				expect(err).to.be(null);
 			});
 		});
-        it('should have written the test message into the output file', function () {
-            sleep(300);
-            expect(fs.readFileSync(filepath, 'utf8')).to.be(test_msg);
+		it('should have written the test message into the output file', function () {
+			sleep(300);
+			expect(fs.readFileSync(filepath, 'utf8')).to.be(test_msg);
 		});
-        it('should be able to shutdown the service', function () {
-            mc.shutdown_service({name: 'fwriter'}, function(err, msg) {
-                expect(err).to.be(null);
-            });
+		it('should be able to shutdown the service', function () {
+			mc.shutdown_service({name: 'fwriter'}, function(err, msg) {
+				expect(err).to.be(null);
+			});
 		});
-        it('services should now be empty', function () {
-            mc.get_services(null, function(err, msg) {
-                expect(err).to.be(null);
-                expect(msg).to.eql({});
-            });
+		it('services should now be empty', function () {
+			mc.get_services(null, function(err, msg) {
+				expect(err).to.be(null);
+				expect(msg).to.eql({});
+			});
 		});
-        it('the pipeline should be inactive', function () {
-            mc.get_pipelines(null, function(err, msg) {
-            	sleep(100);
-                expect(err).to.be(null);
-                expect(msg.pipe.active).to.be(false);
-            });
+		it('the pipeline should be inactive', function () {
+			mc.get_pipelines(null, function(err, msg) {
+				sleep(100);
+				expect(err).to.be(null);
+				expect(msg.pipe.active).to.be(false);
+			});
 		});
-        it('posting to the pipeline should return an error', function () {
-            mc.post_to_pipeline({name: 'pipe', data: 'alfkajdf'}, function(err, msg) {
-                expect(err.code).to.be.greaterThan(399);
-            });
+		it('posting to the pipeline should return an error', function () {
+			mc.post_to_pipeline({name: 'pipe', data: 'alfkajdf'}, function(err, msg) {
+				expect(err.code).to.be.greaterThan(399);
+			});
 		});
 	});
 });
