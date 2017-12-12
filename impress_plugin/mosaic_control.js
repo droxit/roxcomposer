065aedcde42f2786271eb83d45ac4a458da9d739
Sebastian Bickerle <bickerle@droxit.de>
2017-12-12 12:21:46 +0100

    make mosaic_control importable multiple times as separate instances

--- impress_plugin/mosaic_control.js
+++ impress_plugin/mosaic_control.js
@@ -4,23 +4,27 @@ let spawn = require('child_process').spawn;
 let net = require('net');
 let uuid = require('uuid/v4');
 let mosaic_message = require('./service_com_pb.js');
-let processes = {};
-let services = {};
-let pipelines = {};
-let logger;
-let service_container_path;
-let reporting_service;
+
+function __mosaic_control_private() {
+	this.processes = {};
+	this.services = {};
+	this.pipelines = {};
+	this.logger;
+	this.service_container_path;
+	this.reporting_service;
+}
 
 module.exports = function (container) {
-	container['init'] = init;
-	container['start_service'] = start_service;
-	container['shutdown_service'] = shutdown_service;
-	container['get_services'] = get_services;
-	container['get_pipelines'] = get_pipelines;
-	container['set_pipeline'] = set_pipeline;
-	container['post_to_pipeline'] = post_to_pipeline;
-	container['get_msg_history'] = get_msg_history;
-	container['get_msg_status'] = get_msg_status;
+	let mcp = new __mosaic_control_private();
+	container['init'] = init.bind(undefined, mcp);
+	container['start_service'] = start_service.bind(undefined, mcp);
+	container['shutdown_service'] = shutdown_service.bind(undefined, mcp);
+	container['get_services'] = get_services.bind(undefined, mcp);
+	container['get_pipelines'] = get_pipelines.bind(undefined, mcp);
+	container['set_pipeline'] = set_pipeline.bind(undefined, mcp);
+	container['post_to_pipeline'] = post_to_pipeline.bind(undefined, mcp);
+	container['get_msg_history'] = get_msg_history.bind(undefined, mcp);
+	container['get_msg_status'] = get_msg_status.bind(undefined, mcp);
 };
 
 function service_name2id(name) {
@@ -35,16 +39,16 @@ function service_name2id(name) {
  * intialize this plugin
  * args needs to contain IP, port and the beginning of the portrange for the micro services
  **/
-function init(args) {
+function init(mcp, args) {
 	if (args && ('logger' in args))
-		logger = args.logger;
+		mcp.logger = args.logger;
 	else
-		logger = bunyan.createLogger({name: 'mosaic_control'});
+		mcp.logger = bunyan.createLogger({name: 'mosaic_control'});
 
 	if (args && ('service_container' in args))
-		service_container_path = args.service_container;
+		mcp.service_container_path = args.service_container;
 
-	if (service_container_path) {
+	if (mcp.service_container_path) {
 		try {
 			fs.accessSync(service_container_path);
 		} catch (e) {
@@ -54,12 +58,12 @@ function init(args) {
 	}
 
 	if (args && ('reporting_service' in args)) {
-		start_service(args.reporting_service, (err) => {
+		start_service(mcp, args.reporting_service, (err) => {
 			if (err) {
 				let msg = err.message;
 				throw new Error(msg);
 			}
-			reporting_service = args.reporting_service.params.name;
+			mcp.reporting_service = args.reporting_service.params.name;
 		});
 	}
 }
@@ -68,8 +72,8 @@ function init(args) {
  * spawn a new service
  * args need to contain the name of the service and the path to the service module
  **/
-function start_service(args, cb) {
-	logger.debug({args: args}, 'start_service called');
+function start_service(mcp, args, cb) {
+	mcp.logger.debug({args: args}, 'start_service called');
 	let opt;
 
 	if (args === undefined)
@@ -86,8 +90,8 @@ function start_service(args, cb) {
 			return;
 		}
 	} else if ('classpath' in args) {
-        if (service_container_path) {
-		    opt = [service_container_path, args.classpath];
+        if (mcp.service_container_path) {
+		    opt = [mcp.service_container_path, args.classpath];
         } else {
 		    cb({'code': 400, 'message': 'start_service: classpath argument given but path to service loader is not configured'});
 		    return;
@@ -107,7 +111,7 @@ function start_service(args, cb) {
 		return;
 	}
 
-	if ('name' in services) {
+	if ('name' in mcp.services) {
 		cb({'code': 400, 'message': 'start_service: a service with that name already exists'});
 		return;
 	}
@@ -115,49 +119,50 @@ function start_service(args, cb) {
 	let name = args.params.name;
 	let params = args.params;
 	opt.push(JSON.stringify(params));
-	services[name] = {};
-	services[name].path = args.path;
-	services[name].params = args.params;
+	mcp.services[name] = {};
+	mcp.services[name].path = args.path;
+	mcp.services[name].params = args.params;
 
-	logger.debug({opts: opt}, 'spawning process');
+	mcp.logger.debug({opts: opt}, 'spawning process');
 
-	processes[name] = spawn('python3', opt, {stdio: 'inherit'})
+	mcp.processes[name] = spawn('python3', opt, {stdio: 'inherit'})
 		.on('exit', (code, signal) => {
 			// service exit callback
-			logger.info({service: name, exit_code: code}, "service exited");
+			mcp.logger.info({service: name, exit_code: code}, "service exited");
 
 			// if a service receives the signal to shudtdown - set all pipelines to inactive if it conains the service
 			// that shuts down
 			if (signal === 'SIGTERM') {
-				logger.info({service: name, exit_code: code}, "service terminated by user");
-				for (let pl in pipelines) {
-					for (let i=0; i < pipelines[pl]['services'].length; i++) {
-						if (pipelines[pl]['services'][i] === name) {
-							pipelines[pl]['active'] = false;
+				mcp.logger.info({service: name, exit_code: code}, "service terminated by user");
+				for (let pl in mcp.pipelines) {
+					for (let i=0; i < mcp.pipelines[pl]['services'].length; i++) {
+						if (mcp.pipelines[pl]['services'][i] === name) {
+							mcp.pipelines[pl]['active'] = false;
 							break;
 						}
 					}
 				}
 			}
+			console.log(`service ${name} exited`);
 
-			delete processes[name];
-			delete services[name];
+			delete mcp.processes[name];
+			delete mcp.services[name];
 		})
 		.on('error', (e) => {
-            delete services[name];
-			logger.error({error: e, args: args}, "unable to spawn service");
+			delete mcp.services[name];
+			mcp.logger.error({error: e, args: args}, "unable to spawn service");
 		});
 
 	cb(null, {'message': `service [${name}] created`});
 }
 
-function create_mosaic_message(pline, data) {
+function create_mosaic_message(mcp, pline, data) {
 	let msg = new mosaic_message.MosaicMessage();
 	let arr = [];
 	for (let s in pline) {
 		let sname = pline[s];
 		let service = new mosaic_message.Service();
-		service.setId(services[sname]['params'].ip + ":" + services[sname]['params'].port);
+		service.setId(mcp.services[sname]['params'].ip + ":" + mcp.services[sname]['params'].port);
 		arr.push(service);
 	}
 	let pipeline = new mosaic_message.Pipeline();
@@ -172,75 +177,75 @@ function create_mosaic_message(pline, data) {
 }
 
 // args = { 'name': pipeline_name, 'data': "..." }
-function post_to_pipeline(args, cb) {
-	if (!(args.name in pipelines)) {
+function post_to_pipeline(mcp, args, cb) {
+	if (!(args.name in mcp.pipelines)) {
 		cb({'code': 400, 'message': 'no pipeline with that name'});
 		return;
 	}
 
-	let pline = pipelines[args.name];
+	let pline = mcp.pipelines[args.name];
 
     if (pline.active === false) {
 		cb({'code': 400, 'message': `pipeline ${args.name} is inactive`});
 		return;
     }
 
-	let msg = create_mosaic_message(pline.services, args.data);
+	let msg = create_mosaic_message(mcp, pline.services, args.data);
 
 	let socket = new net.Socket();
-	let start = services[pline.services[0]];
+	let start = mcp.services[pline.services[0]];
 	socket.connect({port: start.params.port, host: start.params.ip}, () => {
 		let packet = msg.serializeBinary();
 		socket.end(String.fromCharCode.apply(null, packet));
 		cb(null, {'message': 'pipeline initiated', 'message_id': msg.getId()});
 	});
 	socket.on('error', (e) => {
-		logger.error({error: e}, "unable to connect to service");
+		mcp.logger.error({error: e}, "unable to connect to service");
 		cb({'code': 500, 'message': 'internal server error'});
 	});
 }
 
 // no args required
-function get_services(args, cb) {
-	cb(null, services);
+function get_services(mcp, args, cb) {
+	cb(null, mcp.services);
 }
 
 // no args required
-function get_pipelines(args, cb) {
-	cb(null, pipelines);
+function get_pipelines(mcp, args, cb) {
+	cb(null, mcp.pipelines);
 }
 
 // args = { 'name': "...", 'pipeline': [ ... service names ... ] }
-function set_pipeline(args, cb) {
+function set_pipeline(mcp, args, cb) {
 	if (!('services' in args)) {
 		let msg = 'set_pipeline: service array missing from arguments';
-		logger.error({args: args}, msg);
+		mcp.logger.error({args: args}, msg);
 		cb({'code': 400, 'message': msg});
 		return;
 	}
 	if (!Array.isArray(args.services)) {
 		let msg = 'set_pipeline: args.services must be an array';
-		logger.error({args: args}, msg);
+		mcp.logger.error({args: args}, msg);
 		cb({'code': 400, 'message': msg});
 		return;
 	}
 	if (args.services.length === 0) {
 		let msg = 'set_pipeline: args.services must not be empty';
-		logger.error({args: args}, msg);
+		mcp.logger.error({args: args}, msg);
 		cb({'code': 400, 'message': msg});
 		return;
 	}
 	for (let s in args.services) {
-		if (!(args.services[s] in services)) {
+		if (!(args.services[s] in mcp.services)) {
 			let msg = `set_pipeline: no service with name [${args.services[s]}]`;
-			logger.error(msg);
+			mcp.logger.error(msg);
 			cb({'code': 400, 'message': msg});
 			return;
 		}
 	}
 
 	let pipeline_services = args.services;
-	pipelines[args.name] = {
+	mcp.pipelines[args.name] = {
 		'services': pipeline_services,
 		'active': true
 	};
@@ -248,15 +253,14 @@ function set_pipeline(args, cb) {
 }
 
 // args = { 'name': "..." }
-function shutdown_service(args, cb) {
+function shutdown_service(mcp, args, cb) {
 	if (args === undefined)
 		throw TypeError("shutdown: 'args' must be a dictionary");
 	if (typeof cb !== 'function')
 		throw TypeError("shutdown: 'cb' must be a function");
 
-	if (args.name in services) {
-		let proc = processes[args.name];
-		delete services[args.name];
+	if (args.name in mcp.services) {
+		let proc = mcp.processes[args.name];
 		proc.kill('SIGTERM');
 		cb(null, {'message': 'service stopped'});
 	} else {
@@ -264,7 +268,7 @@ function shutdown_service(args, cb) {
 	}
 }
 
-function post_to_report_service(funcname, args, cb) {
+function post_to_report_service(mcp, funcname, args, cb) {
 	let server = new net.createServer((c) => {
 		// will only be closed when the last active connection has been closed
 		// but this way we don't have to do it later
@@ -284,7 +288,7 @@ function post_to_report_service(funcname, args, cb) {
 		});
 	});
 	server.listen(() => {
-		let socket = net.createConnection( services[reporting_service].params.port, services[reporting_service].params.ip, () => {
+		let socket = net.createConnection( mcp.services[reporting_service].params.port, mcp.services[reporting_service].params.ip, () => {
 			let addr = socket.address();
 			let pline = [ service_name2id(reporting_service), `${server.address().address}:${server.address().port}` ]
 			let msg = create_mosaic_message(pline, JSON.stringify(args));
@@ -293,27 +297,27 @@ function post_to_report_service(funcname, args, cb) {
 			cb(null, {'message': 'pipeline initiated'});
 		}).on('error', (e) => {
 			server.close();
-			logger.error({error: e}, "unable to connect to reporting service");
+			mcp.logger.error({error: e}, "unable to connect to reporting service");
 			cb({'code': 500, 'message': 'internal server error'});
 		});
 	});
 	server.on('error', (e) => {
-		logger.error({error: e}, 'socket listening for report service answer encountered an error');
+		mcp.logger.error({error: e}, 'socket listening for report service answer encountered an error');
 		server.close();
 		cb({'code': 500, 'message': 'internal server error'});
 	});
 }
 
-function get_msg_history(args, cb) {
-	if (reporting_service)
-		post_to_report_service("get_msg_history", args, cb);
+function get_msg_history(mcp, args, cb) {
+	if (mcp.reporting_service)
+		post_to_report_service(mcp, "get_msg_history", args, cb);
 	else
 		cb({'code': 400, 'message': 'no reporting service has been configured'});
 }
 
-function get_msg_status(args, cb) {
-	if (reporting_service)
-		post_to_report_service("get_msg_status", args, cb);
+function get_msg_status(mcp, args, cb) {
+	if (mcp.reporting_service)
+		post_to_report_service(mcp, "get_msg_status", args, cb);
 	else
 		cb({'code': 400, 'message': 'no reporting service has been configured'});
 }
