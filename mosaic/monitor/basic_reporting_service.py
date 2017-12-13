ebe295e43eda44d3fae6f468e365e09d735181a6
Sebastian Bickerle <bickerle@droxit.de>
2017-12-13 13:33:29 +0100

    fix various bugs

--- mosaic/monitor/basic_reporting_service.py
+++ mosaic/monitor/basic_reporting_service.py
@@ -1,22 +1,23 @@
+import json
 from mosaic import base_service
-import basic_monitoring
+from mosaic.monitor import basic_monitoring
 
 ### expects JSON messages in the form {"function": "get_msg_status", "args": {...}}
 class BasicReportingService(base_service.BaseService):
     def __init__(self, params):
         super().__init__(params)
-        check_args(params, "filename")
-        self.reporter = basic_monitoring.BasicReporting(filename=params['filename'])
+        basic_monitoring.check_args(self.params, "filename")
+        self.reporter = basic_monitoring.BasicReporting(filename=self.params['filename'])
 
     def on_message(self, msg):
         try:
-            m = json.parse(msg)
+            m = json.loads(msg)
         except Exception as e:
             errormsg = "unable to parse message, expecting JSON. msg: {}".format(msg)
             self.logger.error(errormsg)
             self.dispatch('{"error": "{}"}'.format(errormsg))
 
-        check_args(m, "function", "args")
+        basic_monitoring.check_args(m, "function", "args")
         try:
             if args['function'] == "get_msg_status":
                 reply = self.reporter.get_msg_status(m['args'])
@@ -27,6 +28,6 @@ class BasicReportingService(base_service.BaseService):
         except Exception as e:
             reply = {"error": str(e)}
 
-        dispatch(json.dumps(reply))
+        self.dispatch(json.dumps(reply))
 
 
