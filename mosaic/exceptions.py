f034e9f29f0efcd44cc77847f4e406a8519b7ee8
Sebastian Bickerle <bickerle@droxit.de>
2017-12-18 11:45:48 +0100

    add exception for too short packet

--- mosaic/exceptions.py
+++ mosaic/exceptions.py
@@ -33,3 +33,10 @@ class MessageLengthMismatch(Exception):
     def __str__(self):
         return repr(self.value)
 
+class InvalidArgument(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
