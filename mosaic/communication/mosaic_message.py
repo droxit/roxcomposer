a640bffaf9d50091141a145b09467c41e3641eec
Sebastian Bickerle <bickerle@droxit.de>
2017-12-18 11:33:07 +0100

    some changes and fixes

--- mosaic/communication/mosaic_message.py
+++ mosaic/communication/mosaic_message.py
@@ -10,7 +10,7 @@ def frame_message(msg):
     return struct.pack('>I', len(msg)) + msg
 
 def unframe_message(msg):
-    l = struct.unpack('>I', msg[:4])
+    l = struct.unpack('>I', msg[:4])[0]
     if l + 4 != len(msg):
         raise exceptions.MessageLengthMismatch('Message of length {} does not match frame header {}'.format(len(msg), l))
     return msg[4:]
@@ -22,24 +22,36 @@ class Service():
         self.parameters = parameters
 
     def encodeId(self):
-        return "{}:{}".format(ip, port)
+        return "{}:{}".format(self.ip, self.port)
 
+    @staticmethod
     def decodeId(idstring):
         parts = idstring.split(':')
         if len(parts) < 2:
-            errmsg = 'invalid service id: {}'.format(next_service)
+            errmsg = 'invalid service id: {}'.format(idstring)
             raise exceptions.InvalidServiceId(errmsg)
-        self.port = int(next_service_id.pop())
-        self.ip = ':'.join(next_service_id)
+        port = int(parts.pop())
+        ip = ':'.join(parts)
+        return (ip, port)
+
+    def __str__(self):
+        return str({ 'ip': self.ip, 'port': self.port, 'parameters': self.parameters })
+
+    def __repr__(self):
+        return '<Service ' + str(self) + '>'
+
+    def __eq__(self, other):
+        return repr(self) == repr(other)
 
 class Message():
     def __init__(self):
         self.pipeline = []
         self.id = None
         self.payload = None
+        self.create_id()
 
     def create_id(self):
-        self.id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
 
     def set_payload(self, data):
         self.payload = data
@@ -49,72 +61,71 @@ class Message():
 
     def get_content_as_dict(self):
         return {
-                'pipeline': self.pipeline,
                 'id': self.id,
+                'pipeline': self.pipeline,
                 'payload': self.payload
             }
 
     # raises KeyError if the pipeline is empty
     def pop_service(self):
-        return pipeline.pop(0)
+        return self.pipeline.pop(0)
 
     def add_service(self, service):
         self.pipeline.append(service)
 
-    def serializeToProtobuf(self):
+    def serialize_to_protobuf(self):
         pmsg = proto.MosaicMessage()
         for p in self.pipeline:
-            s = proto.Service()
-            s.id = p.encodeAdress()
+            s = pmsg.pipeline.services.add()
+            s.id = p.encodeId()
             for para in p.parameters:
-                s.parameters.append(para)
-            msg.pipeline.services.append(s)
+                param = s.parameters.add()
+                param.serviceParams = para
 
         pmsg.id = self.id
         pmsg.payload.body = self.payload
-        binmsg = msg.SerializeToString()
-
-        return struct.pack('>I', len(binmsg)) + binmsg
+        binmsg = pmsg.SerializeToString()
+        return binmsg
 
     @staticmethod
-    def deserializeFromProtobuf(binmsg):
+    def deserialize_from_protobuf(binmsg):
         msg = Message()
         pmsg = proto.MosaicMessage()
-        pmsg.parseFromString(binmsg)
-        msg.set_content(pmsg.payload.string)
-        self.id = pmsg.id
-        for s in pmsg.pipeline:
+        pmsg.ParseFromString(binmsg)
+        msg.set_payload(pmsg.payload.body)
+        msg.id = pmsg.id
+        for s in pmsg.pipeline.services:
             ip, port = Service.decodeId(s.id)
             msg.add_service(Service(ip, port, s.parameters))
 
         return msg
 
-    def serializeToJson(self):
+    def serialize_to_json(self):
         msg = {
                 'id': self.id,
-                'pipeline': [x for x in map(lambda s: { 'id': s.encode(), 'parameters': s.parameters }, self.pipeline)],
+                'pipeline': [x for x in map(lambda s: { 'id': s.encodeId(), 'parameters': s.parameters }, self.pipeline)],
                 'payload': self.payload
             }
 
         return json.dumps(msg)
 
     @staticmethod
-    def deserializeFromJson(jsonstring):
+    def deserialize_from_json(jsonstring):
         msg = Message()
         d = json.loads(jsonstring)
-        msg.set_content(d['payload'])
+        msg.set_payload(d['payload'])
         msg.id = d['id']
         for s in d['pipeline']:
-            ip, port = Service.decodeId(s.id)
+            ip, port = Service.decodeId(s['id'])
             msg.add_service(Service(ip, port, s['parameters']))
 
         return msg
 
 
     def serialize(self):
-        return frame_message(self.serializeToProtobuf())
+        return frame_message(self.serialize_to_protobuf())
 
     @staticmethod
     def deserialize(msg):
-        return deserializeFromProtobuf(unframe_message(msg))
+        return Message.deserialize_from_protobuf(unframe_message(msg))
         
