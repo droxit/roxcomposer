8f7a1a523048d856d0107d603004a57e015fe9c5
villarroya <villarroya@droxit.de>
2018-01-30 15:45:53 +0100

    adds local scripts for build and deploy

---
+++ deploy_mosaic_local.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+MOSAIC_DEPLOY_DIR=$1
+DEPLOY_LOG_DIR=$MOSAIC_DEPLOY_DIR/logs
+
+mkdir $DEPLOY_LOG_DIR
+cp mosaic-demo-*.tar.gz $MOSAIC_DEPLOY_DIR/.
+
+cd $MOSAIC_DEPLOY_DIR
+
+rm -rf mosaic_build
+tar xf mosaic-demo*.tar.gz
+cd mosaic_build
+pip3.6 uninstall mosaic
+#pip3.6 install --user packages/mosaic-0.2.0.tar.gz > $DEPLOY_LOG_DIR/mosaic_install.log
+./install.sh --user > $DEPLOY_LOG_DIR/mosaic_install.log
+#./start_server.sh
