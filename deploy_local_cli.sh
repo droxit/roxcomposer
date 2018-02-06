a0a759b484aa5df8ccea12024b8c7d79f957f58b
villarroya <villarroya@droxit.de>
2018-02-06 15:11:23 +0100

    new version 0.2.4.x

---
+++ deploy_local_cli.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#
+# script to local mosaic-cli deploy
+# devs@droxit.de - droxIT GmbH
+#
+# Copyright (c) 2018 droxIT GmbH
+#
+
+MOSAIC_DEPLOY_DIR=$1
+BUILD_DIR="mosaic_demo"
+
+function usage {
+    echo "usage: deploy_local_cli <TO_INSTALL_DEMO_PATH>"
+    exit 0
+}
+
+### EXECUTION ###
+if [[ ($# -lt 1) ]]
+    then usage
+else
+    rm -rf $MOSAIC_DEPLOY_DIR/$BUILD_DIR/mosaic-cli*
+    cp mosaic-cli $MOSAIC_DEPLOY_DIR/$BUILD_DIR/.
+
+fi
