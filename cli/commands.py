6aa5b65447367a5b0c95862d59a4532ed7a36697
Sebastian Bickerle <bickerle@droxit.de>
2018-04-09 16:46:03 +0200

    add command module

---
+++ cli/commands.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python3
+#
+# commands.py
+#
+# devs@droxit.de - droxIT GmbH
+#
+# Copyright (c) 2018 droxIT GmbH
+#
+
+import json
+import os
+import requests
+
+roxconnector = 'localhost:7475'
+service_file_dir = 'services'
+cmd_map = {}
+
+def list_service_files(*args):
+    ret = []
+    for f in os.scandir(service_file_dir):
+        if f.is_file() and f.name.endswith('.json'):
+            ret.append(f.name)
+
+    return ret
+
+def get_services(*args):
+    r = requests.get('http://{}/services'.format(roxconnector))
+    if r.status_code == 200:
+        return r.text
+    else:
+        return 'ERROR: {} - {}'.format(r.status_code, r.text)
+    
+def list_commands(*args):
+    return "available commands: \n\t" + "\n\t".join([x for x in cmd_map])
+
+
+cmd_map = {
+        'list_service_files': list_service_files,
+        'services': get_services,
+        'help': list_commands
+}
+
+def run_cmd(*args):
+    if len(args) == 0:
+        raise RuntimeError('no command given')
+    if args[0] not in cmd_map:
+        raise RuntimeError("command '{}' is not defined".format(args[0]))
+
+    return cmd_map[args[0]](args[1:])
+
