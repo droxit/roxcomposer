bbbb9f047aea6af44c2391aba31439c34a72c245
Sebastian Bickerle <bickerle@droxit.de>
2018-06-25 11:06:24 +0200

    fix version.sh for circleci builds

--- version.sh
+++ version.sh
@@ -1,6 +1,9 @@
 BRANCH=`git branch | grep \* | cut -d ' ' -f2 | tr -d '_-'`
 VERSION=`cat VERSION`
 
+# on CircleCI the BRANCH variable is not set correctly
+if [ ! $BRANCH ]; then BRANCH=$CIRCLE_BRANCH; fi
+
 case $BRANCH in
 	"master")
 		echo -n $VERSION
