f21a82b1b097c24a0adc2d35f9fe02db8cbefc48
Sebastian Bickerle <bickerle@droxit.de>
2018-06-26 09:39:23 +0200

    fix pipeline activation

--- roxconnector_plugin/roxcomposer_control.js
+++ roxconnector_plugin/roxcomposer_control.js
@@ -286,10 +286,10 @@ function start_service(args, cb) {
 
     //activate all pipelines that include this service and have no inactive services
 	Object.entries(this.pipelines)
-		.filter((pname, x) => x.active === false)
-		.filter((pname, x) => x.indexOf(name) != -1)
-		.forEach((pname, srvs) => {
-			let should_be_active = srvs.map(x => (x in this.services) && this.services[x].active ).reduce( (x, y) => x && y, true )
+		.filter(([pname, x]) => x.active === false)
+		.filter(([pname, x]) => x.services.indexOf(name) != -1)
+		.forEach(([pname, x]) => {
+			let should_be_active = x.services.map(x => x in this.services).reduce( (x, y) => x && y, true );
 			if (should_be_active)
 				this.pipelines[pname].active = true;
 		});
