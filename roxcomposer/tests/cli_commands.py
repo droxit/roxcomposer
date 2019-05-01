5b6b68befa80d082de3803ba6979e05262a057d5
Markus Baumgärtner <baumgaertner@droxit.de>
2019-05-01 11:08:38 +0200

    Updated open source disclaimer to ROXcomposer files

--- roxcomposer/tests/cli_commands.py
+++ roxcomposer/tests/cli_commands.py
@@ -6,7 +6,9 @@
 # |                                                                      |
 # | Copyright (C) 2019  droxIT GmbH - devs@droxit.de                     |
 # |                                                                      |
-# | This program is free software: you can redistribute it and/or modify |
+# | This file is part of ROXcomposer.                                    |
+# |                                                                      |
+# | ROXcomposer is free software: you can redistribute it and/or modify  |
 # | it under the terms of the GNU General Public License as published by |
 # | the Free Software Foundation, either version 3 of the License, or    |
 # | (at your option) any later version.                                  |
