f6cbf61f3a86682ee3835d6711f03dd2d71950cb
Markus Baumgärtner <baumgaertner@droxit.de>
2019-05-01 10:48:55 +0200

    Added open source disclaimer to roxcomposer files

--- roxcomposer/base_service.py
+++ roxcomposer/base_service.py
@@ -6,9 +6,24 @@
 # predefined pipeline structure. That means every service which is listed in a pipeline will get and send a message in
 # the defined direction.
 #
-# devs@droxit.de
-#
-# Copyright (c) 2017 droxIT GmbH
+# |------------------- OPEN SOURCE LICENSE DISCLAIMER -------------------|
+# |                                                                      |
+# | Copyright (C) 2019  droxIT GmbH - devs@droxit.de                     |
+# |                                                                      |
+# | This program is free software: you can redistribute it and/or modify |
+# | it under the terms of the GNU General Public License as published by |
+# | the Free Software Foundation, either version 3 of the License, or    |
+# | (at your option) any later version.                                  |
+# |                                                                      |
+# | This program is distributed in the hope that it will be useful,      |
+# | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+# | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the         |
+# | GNU General Public License for more details.                         |
+# |                                                                      |
+# | You have received a copy of the GNU General Public License           |
+# | along with this program. See also <http://www.gnu.org/licenses/>.    |
+# |                                                                      |
+# |----------------------------------------------------------------------|
 #
 
 
--- roxcomposer/monitor/basic_monitoring.py
+++ roxcomposer/monitor/basic_monitoring.py
@@ -4,9 +4,24 @@
 # The basic version of the monitoring writes every information to one file, which is configurable by defining a para-
 # meter in the service parameters.
 #
-# devs@droxit.de
-#
-# Copyright (c) 2018 droxIT GmbH
+# |------------------- OPEN SOURCE LICENSE DISCLAIMER -------------------|
+# |                                                                      |
+# | Copyright (C) 2019  droxIT GmbH - devs@droxit.de                     |
+# |                                                                      |
+# | This program is free software: you can redistribute it and/or modify |
+# | it under the terms of the GNU General Public License as published by |
+# | the Free Software Foundation, either version 3 of the License, or    |
+# | (at your option) any later version.                                  |
+# |                                                                      |
+# | This program is distributed in the hope that it will be useful,      |
+# | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+# | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the         |
+# | GNU General Public License for more details.                         |
+# |                                                                      |
+# | You have received a copy of the GNU General Public License           |
+# | along with this program. See also <http://www.gnu.org/licenses/>.    |
+# |                                                                      |
+# |----------------------------------------------------------------------|
 #
 
 import time
