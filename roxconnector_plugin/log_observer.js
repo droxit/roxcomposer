437654399ed08c244a1ea6817b73c0e8023032a7
Markus Baumgärtner <baumgaertner@droxit.de>
2019-05-02 11:50:39 +0200

    Changed License to LGLP with all needed changes

--- roxconnector_plugin/log_observer.js
+++ roxconnector_plugin/log_observer.js
@@ -1,34 +1,40 @@
-//
-// LogObserver - Watch logfiles and notify registered callbacks
-//
-// usage: const logobs = require('./log_observer');
-//        let observer = new logobs(callback);
-//        observer.register(file1, file2, ...);
-//        /* callback will be called with new log lines as they arrive */
-//        /* unregister when you're done */
-//        observer.unregister(file1, file2, ...);
-//
-// |------------------- OPEN SOURCE LICENSE DISCLAIMER -------------------|
-// |                                                                      |
-// | Copyright (C) 2019  droxIT GmbH - devs@droxit.de                     |
-// |                                                                      |
-// | This file is part of ROXcomposer.                                    |
-// |                                                                      |
-// | ROXcomposer is free software: you can redistribute it and/or modify  |
-// | it under the terms of the GNU General Public License as published by |
-// | the Free Software Foundation, either version 3 of the License, or    |
-// | (at your option) any later version.                                  |
-// |                                                                      |
-// | This program is distributed in the hope that it will be useful,      |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the         |
-// | GNU General Public License for more details.                         |
-// |                                                                      |
-// | You have received a copy of the GNU General Public License           |
-// | along with this program. See also <http://www.gnu.org/licenses/>.    |
-// |                                                                      |
-// |----------------------------------------------------------------------|
-//
+/*
+ * encoding: utf-8
+ *
+ * LogObserver - Watch logfiles and notify registered callbacks
+ * usage: const logobs = require('./log_observer');
+ *        let observer = new logobs(callback);
+ *        observer.register(file1, file2, ...);
+ *        callback will be called with new log lines as they arrive
+ *	      unregister when you're done
+ * observer.unregister(file1, file2, ...);
+ *
+ * This module provides config-loading support similar to the roxcomposer/config/config_loader.py in python.
+ * This is used as a workaround to load needed service information like ip and port until we have
+ * implemented a service callback feature with which the services will report their active configuration
+ * on their own.
+ *
+ * |------------------- OPEN SOURCE LICENSE DISCLAIMER -------------------|
+ * |                                                                      |
+ * | Copyright (C) 2019  droxIT GmbH - devs@droxit.de                     |
+ * |                                                                      |
+ * | This file is part of ROXcomposer.                                    |
+ * |                                                                      |
+ * | ROXcomposer is free software: you can redistribute it and/or modify  |
+ * | it under the terms of the GNU General Public License as published by |
+ * | the Free Software Foundation, either version 3 of the License, or    |
+ * | (at your option) any later version.                                  |
+ * |                                                                      |
+ * | This program is distributed in the hope that it will be useful,      |
+ * | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+ * | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the         |
+ * | GNU General Public License for more details.                         |
+ * |                                                                      |
+ * | You have received a copy of the GNU General Public License           |
+ * | along with this program. See also <http:www.gnu.org/licenses/>.      |
+ * |                                                                      |
+ * |----------------------------------------------------------------------|
+ */
 
 
 let fs = require('fs');
